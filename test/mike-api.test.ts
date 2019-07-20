e4070966d7838fede39c1735e8b1f58cba58a23d
Mike Solomon <mike@meeshkan.com>
2019-07-20 12:42:46 +0300

    Adds throw

--- test/mike-api.test.ts
+++ test/mike-api.test.ts
@@ -1,11 +1,27 @@
 import foo from "../src/mike-api";
 import * as unmock from "unmock-node";
 
-beforeAll(() => {
-    unmock.on();
+let state: any;
+
+beforeEach(() => {
+    state = unmock.on();
+})
+
+afterEach(() => {
+    state = unmock.off();
 })
 
 test("my-api-works", async () => {
+    state.foo('/hello', { $code: 200 })
     const res = await foo();
     expect(Object.keys(res)).toEqual(['hello']);
+})
+
+test("my-api-does-not-work", async () => {
+    state.foo('/hello', { $code: 400 })
+    try {
+        const res = await foo();
+        expect("should have thrown").toBe(true);
+    } catch {
+    }
 })
