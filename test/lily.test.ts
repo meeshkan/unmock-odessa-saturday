c0aa4bde309bb3aa926bf6eb44d3e10367a7645c
lkarpenko <lkarpenko@alteryx.com>
2019-07-20 11:57:36 +0300

    create fake api call

--- test/lily.test.ts
+++ test/lily.test.ts
@@ -1,5 +1,11 @@
 import pow from './src/lily';
+import getUsers from './src/lily-api';
 
-test("check if 1+1=2", () => {
+test("check if pow works", () => {
   expect(pow(2,3)).toEqual(8);
+});
+
+test("fake API call that fails", async () => {
+  const result = await getUsers();
+  expect(result).toEqual({ "name": "Some name" });
 });
